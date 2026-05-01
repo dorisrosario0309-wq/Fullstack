c88fc2c8c9aecf62af6c77bf42231f2998863637
Doris R <doris0309@gmail.com>
2026-05-01 10:47:08 -0400

    Referencias de puertos

--- Index.js
+++ Index.js
@@ -1,1 +1,18 @@
-require('.env').config()
+require('dotenv').config()
+const express=require('express')
+
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const app = express()
+const PORT = process.env.PORT || 3000
+ 
+app.engine(
+    'hbs',
+    exphbs.engine({
+        extname:'hbs',
+        partialsDir: path.join(__dirname, 'VIEWS/PARTIALS'),
+        layoutsDir: path.join(__dirname, 'VIEWS/LAYOUTS'),
+        defaultLayout: 'home.hbs'
+    })
+);
